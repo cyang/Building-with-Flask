34e32771fb895d2b0e2eb562e0fd8a2fca4370ca
Christopher Yang <cyang001@citymail.cuny.edu>
2015-10-18 15:49:08 -0400

    Add templates, control flow, and inheritance

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, redirect, url_for, send_from_directory
+from flask import Flask, request, redirect, url_for, send_from_directory, render_template
 from werkzeug import secure_filename
 
 UPLOAD_FOLDER = '/Users/ChrisYang/Projects/Flask/uploads'
@@ -7,12 +7,25 @@ ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+index_bool=False
+
+@app.route('/')
+def index():
+    return render_template('index_base.html', title="Index page", some_text = "This is the index page GUYS!", index_bool="True")
+
+@app.route('/nicholas')
+def nicholas_cage():
+    return render_template('nicholas.html', title="Nicholas CAGE", some_text = "We are here for the A's", some_link="/", image = "https://cdn3.vox-cdn.com/thumbor/IE_0CtHTpjW1ix_Ztb5VHAdBHOE=/assets.sbnation.com/uploads/chorus_asset/file/2317732/nicolasCage_NotTheBees.0.gif")
+
+@app.route('/panda')
+def hello():
+    return render_template('hello.html', title="Hello page", some_text= "Hello, I'm inheriting from index_base.html", some_link="/" , image="http://vignette2.wikia.nocookie.net/glee/images/7/7f/Panda_says_hi.gif/revision/latest?cb=20130503213951")
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         file = request.files['file']
